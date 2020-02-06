605265423bd0eb5ec152fd2440d2fdbea597cd79
Giulio Zausa <giulio.zausa@smc.it>
2020-02-06 18:43:51 +0100

    Typescript support for prettier

--- src/codeUtils.tsx
+++ src/codeUtils.tsx
@@ -1,8 +1,9 @@
 import * as prettier from "prettier/standalone";
-import parserBabel from "prettier/parser-babylon";
+import parserTypeScript from "prettier/parser-typescript";
 import * as Babel from "@babel/standalone";
 import preset_env from "@babel/preset-env";
 import preset_typescript from "@babel/preset-typescript";
+import * as ts from "typescript";
 import protect from "./babel-plugin-transform-prevent-infinite-loops";
 
 function instrumenter({ types, template }) {
@@ -52,8 +53,8 @@ export function transformCode(source: string) {
 
 export function formatCode(code: string) {
   return prettier.format(code, {
-    parser: "babel",
-    plugins: [parserBabel],
+    parser: "typescript",
+    plugins: [parserTypeScript],
   });
 }
 
@@ -101,3 +102,87 @@ export function getFunctionFromCode<THandler>(code: string) {
   const F = new Function(final)() as THandler;
   return F;
 }
+
+// class CheckerCompilerHost implements ts.CompilerHost {
+//   files: { [fileName: string]: string } = {};
+
+//   getSourceFile(
+//     filename: string,
+//     languageVersion: ts.ScriptTarget,
+//     onError?: (message: string) => void
+//   ): ts.SourceFile {
+//     const text = this.files[filename];
+//     if (!text) return null;
+//     return ts.createSourceFile(filename, text, languageVersion);
+//   }
+//   getDefaultLibFileName = (options: ts.CompilerOptions) => "lib.d.ts";
+//   getDirectories = (path: string) => [];
+
+//   writeFile(
+//     filename: string,
+//     data: string,
+//     writeByteOrderMark: boolean,
+//     onError?: (message: string) => void
+//   ): void {}
+//   getCurrentDirectory = () => "";
+//   getCanonicalFileName = (fileName: string) => fileName;
+//   useCaseSensitiveFileNames = () => true;
+//   getNewLine = () => "\n";
+//   fileExists = (fileName: string) => !!this.files[fileName];
+//   readFile = (fileName: string) => this.files[fileName];
+
+//   addFile(fileName: string, body: string) {
+//     this.files[fileName] = body;
+//   }
+// }
+
+// const findNode = (n: ts.Node, f: (testNode: ts.Node) => Boolean) => {
+//   let result: ts.Node;
+//   function findNode(nn: ts.Node) {
+//     if (result) {
+//       return;
+//     }
+//     if (f(nn)) {
+//       result = nn;
+//       return;
+//     }
+//     ts.forEachChild(nn, findNode);
+//   }
+//   findNode(n);
+//   return result;
+// };
+
+// export function compile(scriptName: string, options: ts.CompilerOptions) {
+//   const host = new CheckerCompilerHost();
+//   host.addFile(scriptName, "const value = 42");
+
+//   const program = ts.createProgram([scriptName], options, host);
+//   const typeChecker = program.getTypeChecker();
+
+//   const sf = program.getSourceFile(scriptName);
+//   const diagnostics = [].concat(
+//     program.getGlobalDiagnostics(),
+//     program.getSyntacticDiagnostics(sf),
+//     program.getSemanticDiagnostics(sf)
+//   );
+
+//   if (diagnostics.length > 0) {
+//     const result = diagnostics.map(d => d.messageText).join("\n");
+//     return { error: true, output: result, type: null };
+//   } else {
+//     console.log(typeChecker.getSymbolsInScope(sf, ts.SymbolFlags.Variable)[0]);
+//     const node = findNode(
+//       sf,
+//       (n: ts.Node) => n.kind === ts.SyntaxKind.VariableDeclaration
+//     );
+//     return {
+//       error: false,
+//       type: typeChecker.typeToString(typeChecker.getTypeAtLocation(node)),
+//     };
+//   }
+// }
+
+// const settings = ts.getDefaultCompilerOptions();
+// settings.diagnostics = true;
+// settings.skipLibCheck = true;
+// console.log(compile("test.ts", settings));
