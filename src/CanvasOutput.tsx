277f133f5382e187744d7229f92ee7cc7671e4b5
Giulio Zausa <giulio.zausa@smc.it>
2020-02-24 20:57:21 +0100

    Fixed wrong reading for canvas

--- src/CanvasOutput.tsx
+++ src/CanvasOutput.tsx
@@ -14,6 +14,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     height: "100%",
     display: "flex",
     justifyContent: "center",
+    alignItems: "center",
   },
   canvas: {
     maxWidth: "100%",
