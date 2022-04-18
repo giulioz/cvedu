6bf47adeaab95bb436da8fec3c560d80992bd483
Giulio Zausa <mail.zausa.giulio@gmail.com>
2022-04-19 01:03:21 +0200

    Added vite react

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   define: {
     'process.env': {},
     Buffer: { isBuffer: () => false },
   },
+  plugins: [react()],
 });
